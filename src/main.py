15acf8388ee8cf1d9a3fc2487a1439f0e4d041be
bryankeane0 <20089166@mail.wit.ie>
2021-12-12 22:07:25 +0000

    Created grid.py and added conway methods for algorithm

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import pygame
+import os
+
+os.environ["SDL_VIDEO_CENTERED"]='1'
+
+width, height = 1920, 1080
+size = (width, height)
+
+pygame.init()
+pygame.display.set_caption("Conway's Game of Life!")
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+fps = 60
+black = (0, 0, 0)
+blue = (0, 14, 71)
+white = (255, 255, 255)
